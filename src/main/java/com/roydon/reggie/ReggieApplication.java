6033b41aff6fc0fa74ff6566e0f2e477ae9b69e1
roydonGuo <3133010060@qq.com>
2022-09-27 18:32:10 +0800

    ServletComponentScan扫描过滤器

--- src/main/java/com/roydon/reggie/ReggieApplication.java
+++ src/main/java/com/roydon/reggie/ReggieApplication.java
@@ -3,6 +3,7 @@ package com.roydon.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Created by Intellij IDEA
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
