ab4a6f3c659168d85b6780820c43f5b5a0eee914
roydonGuo <3133010060@qq.com>
2022-09-28 20:59:58 +0800

    过滤器

--- src/main/java/com/roydon/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/roydon/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.roydon.reggie.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.roydon.reggie.common.BaseContext;
 import com.roydon.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
@@ -56,6 +57,10 @@ public class LoginCheckFilter implements Filter {
         //4、判断登录状态，如果已登录，则直接放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
+
+            Long id = (Long) request.getSession().getAttribute("employee");
+            BaseContext.setCurrentId(id);
+
             filterChain.doFilter(request, response);
             return;
         }
