c14b9498e80d675a13f84850c7cd2c5bf11b6112
roydonGuo <3133010060@qq.com>
2022-09-29 21:43:43 +0800

    菜品喜好

---
+++ src/main/java/com/roydon/reggie/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,13 @@
+package com.roydon.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.roydon.reggie.entity.DishFlavor;
+import com.roydon.reggie.mapper.DishFlavorMapper;
+import com.roydon.reggie.service.DishFlavorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+
+
+}
