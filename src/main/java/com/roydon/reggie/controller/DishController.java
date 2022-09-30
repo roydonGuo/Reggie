25386e1091ed221071d43737766e5c588758c49e
roydonGuo <3133010060@qq.com>
2022-09-30 14:21:06 +0800

    菜品接口iIIi

---
+++ src/main/java/com/roydon/reggie/controller/DishController.java
@@ -0,0 +1,18 @@
+package com.roydon.reggie.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/29
+ * Time: 21:25
+ **/
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+
+
+
+}
