22f8243776f8479903829ce2531787f2399c261f
roydonGuo <3133010060@qq.com>
2022-09-30 22:37:15 +0800

    菜品接口层

--- src/main/java/com/roydon/reggie/controller/DishController.java
+++ src/main/java/com/roydon/reggie/controller/DishController.java
@@ -1,18 +1,95 @@
 package com.roydon.reggie.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.roydon.reggie.common.R;
+import com.roydon.reggie.entity.Dish;
+import com.roydon.reggie.entity.dto.DishDto;
+import com.roydon.reggie.service.DishFlavorService;
+import com.roydon.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Intellij IDEA
  * Author: yi cheng
  * Date: 2022/9/29
  * Time: 21:25
+ * <p>
+ * 菜品管理
  **/
+@Slf4j
 @RestController
 @RequestMapping("/dish")
 public class DishController {
 
+    @Resource
+    private DishService dishService;
+
+    @Resource
+    private DishFlavorService dishFlavorService;
+
+    /**
+     * 新增菜品
+     *
+     * @param dishDto
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody DishDto dishDto) {
+        log.info(dishDto.toString());
+
+        dishService.saveWithFlavor(dishDto);
+
+        return R.success("新增菜品成功");
+    }
+
+    /**
+     * 分页展示菜品
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R getPage(@RequestParam("page") Integer pageNum,
+                     @RequestParam Integer pageSize) {
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+        return R.success(dishService.page(new Page<>(pageNum, pageSize), queryWrapper));
+
+    }
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<DishDto> get(@PathVariable Long id) {
+
+        DishDto dishDto = dishService.getByIdWithFlavor(id);
+
+        return R.success(dishDto);
+    }
+
+    /**
+     * 修改菜品
+     *
+     * @param dishDto
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody DishDto dishDto) {
+        log.info(dishDto.toString());
+
+        dishService.updateWithFlavor(dishDto);
+
+        return R.success("新增菜品成功");
+    }
 
 
 }
