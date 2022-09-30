4468bc609b3769fb3b41d4cc1b74527feac35af1
roydonGuo <3133010060@qq.com>
2022-09-30 14:24:16 +0800

    Setmeal

---
+++ src/main/java/com/roydon/reggie/service/SetmealService.java
@@ -0,0 +1,7 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.Setmeal;
+
+public interface SetmealService extends IService<Setmeal> {
+}
