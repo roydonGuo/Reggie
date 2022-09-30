cf1877dc8361bf7ec3a635b5f915704817ecfa2c
roydonGuo <3133010060@qq.com>
2022-09-30 14:23:31 +0800

    菜品

---
+++ src/main/java/com/roydon/reggie/service/DishService.java
@@ -0,0 +1,7 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.Dish;
+
+public interface DishService extends IService<Dish> {
+}
