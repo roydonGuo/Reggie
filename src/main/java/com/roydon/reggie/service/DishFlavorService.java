2d79f67a9d100461d4dc358cd28567306ab8064a
roydonGuo <3133010060@qq.com>
2022-09-30 14:23:15 +0800

    口味

---
+++ src/main/java/com/roydon/reggie/service/DishFlavorService.java
@@ -0,0 +1,9 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.DishFlavor;
+
+public interface DishFlavorService extends IService<DishFlavor> {
+
+
+}
