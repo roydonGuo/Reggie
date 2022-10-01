c4bbbb4ac2acd6791a2db4c5c91ef5224f617d8b
roydonGuo <3133010060@qq.com>
2022-10-01 19:22:43 +0800

    新增移动端逻辑##+

--- src/main/java/com/roydon/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/roydon/reggie/filter/LoginCheckFilter.java
@@ -43,7 +43,9 @@ public class LoginCheckFilter implements Filter {
                 "/employee/logout",
                 "/backend/**",
                 "/front/**",
-                "/common/**"
+                "/common/**",
+                "/user/sendMsg",
+                "/user/login"
         };
         //2、判断本次请求是否需要处理
         boolean check = check(urls, requestURI);
@@ -55,12 +57,22 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //4、判断登录状态，如果已登录，则直接放行
+        //4-1、判断登录状态，如果已登录，则直接放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
 
             Long id = (Long) request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(id);
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //4-2、判断移动端登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("user") != null) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
 
             filterChain.doFilter(request, response);
             return;
