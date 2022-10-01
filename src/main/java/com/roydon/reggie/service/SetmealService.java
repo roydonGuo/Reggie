08b41cfe17af219a2f0cd8709136e0c80202936a
roydonGuo <3133010060@qq.com>
2022-10-01 19:21:46 +0800

    套餐 service

--- src/main/java/com/roydon/reggie/service/SetmealService.java
+++ src/main/java/com/roydon/reggie/service/SetmealService.java
@@ -2,6 +2,22 @@ package com.roydon.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.roydon.reggie.entity.Setmeal;
+import com.roydon.reggie.entity.dto.SetmealDto;
+
+import java.util.List;
 
 public interface SetmealService extends IService<Setmeal> {
+    /**
+     * 新增套餐，同时需要保存套餐和菜品的关联关系
+     *
+     * @param setmealDto
+     */
+    public void saveWithDish(SetmealDto setmealDto);
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     *
+     * @param ids
+     */
+    public void removeWithDish(List<Long> ids);
 }
