e972e8fee907bd525a8af9cd5fd5f81c5e5a14ca
roydonGuo <3133010060@qq.com>
2022-10-01 22:34:10 +0800

    AddressBookController

---
+++ src/main/java/com/roydon/reggie/controller/AddressBookController.java
@@ -0,0 +1,136 @@
+package com.roydon.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.roydon.reggie.common.BaseContext;
+import com.roydon.reggie.common.R;
+import com.roydon.reggie.entity.AddressBook;
+import com.roydon.reggie.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/10/1
+ * Time: 19:32
+ **/
+@RestController
+@Slf4j
+@RequestMapping("/addressBook")
+public class AddressBookController {
+
+    @Resource
+    private AddressBookService addressBookService;
+
+    /**
+     * 新增
+     *
+     * @param addressBook
+     * @return
+     */
+    @PostMapping
+    public R<AddressBook> save(@RequestBody AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        log.info("addressBook:{}", addressBook);
+        addressBookService.save(addressBook);
+        return R.success(addressBook);
+    }
+
+    /**
+     * 修改地址
+     *
+     * @param addressBook
+     * @return
+     */
+    @PutMapping()
+    public R update(@RequestBody AddressBook addressBook) {
+        return R.success(addressBookService.updateById(addressBook));
+    }
+
+    /**
+     * 删除地址
+     *
+     * @param ids
+     * @return
+     */
+    @DeleteMapping()
+    public R delete(@RequestParam List<Long> ids) {
+
+        return R.success(addressBookService.removeBatchByIds(ids));
+
+    }
+
+    /**
+     * 设置默认地址
+     */
+    @PutMapping("default")
+    public R<AddressBook> setDefault(@RequestBody AddressBook addressBook) {
+        log.info("addressBook:{}", addressBook);
+        // 该用户其他的地址默认值设0
+        LambdaUpdateWrapper<AddressBook> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
+        wrapper.set(AddressBook::getIsDefault, 0);
+        //SQL:update address_book set is_default = 0 where user_id = ?
+        addressBookService.update(wrapper);
+        // 该用户此地址设为默认
+        addressBook.setIsDefault(1);
+        //SQL:update address_book set is_default = 1 where id = ?
+        addressBookService.updateById(addressBook);
+        return R.success(addressBook);
+    }
+
+
+    /**
+     * 根据id查询地址
+     */
+    @GetMapping("/{id}")
+    public R get(@PathVariable Long id) {
+        AddressBook addressBook = addressBookService.getById(id);
+        if (addressBook != null) {
+            return R.success(addressBook);
+        } else {
+            return R.error("没有找到该对象");
+        }
+    }
+
+    /**
+     * 查询默认地址
+     */
+    @GetMapping("default")
+    public R<AddressBook> getDefault() {
+        LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
+        queryWrapper.eq(AddressBook::getIsDefault, 1);
+
+        //SQL:select * from address_book where user_id = ? and is_default = 1
+        AddressBook addressBook = addressBookService.getOne(queryWrapper);
+
+        if (null == addressBook) {
+            return R.error("没有找到该对象");
+        } else {
+            return R.success(addressBook);
+        }
+    }
+
+    /**
+     * 查询指定用户的全部地址
+     */
+    @GetMapping("/list")
+    public R<List<AddressBook>> list(AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        log.info("addressBook:{}", addressBook);
+
+        //条件构造器
+        LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(null != addressBook.getUserId(), AddressBook::getUserId, addressBook.getUserId());
+        queryWrapper.orderByDesc(AddressBook::getUpdateTime);
+
+        //SQL:select * from address_book where user_id = ? order by update_time desc
+        return R.success(addressBookService.list(queryWrapper));
+    }
+
+}
