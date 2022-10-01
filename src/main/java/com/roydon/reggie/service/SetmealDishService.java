e46cfecc2cd37df8549c498f90f89f743e9d50ed
roydonGuo <3133010060@qq.com>
2022-10-01 19:22:15 +0800

    套餐菜品 service

---
+++ src/main/java/com/roydon/reggie/service/SetmealDishService.java
@@ -0,0 +1,7 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.SetmealDish;
+
+public interface SetmealDishService extends IService<SetmealDish> {
+}
