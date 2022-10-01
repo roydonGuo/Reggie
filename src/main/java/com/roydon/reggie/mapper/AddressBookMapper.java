9b601021d92404129c86a3c67ff91a1a53ff3882
roydonGuo <3133010060@qq.com>
2022-10-01 22:35:27 +0800

    地址 mapper

---
+++ src/main/java/com/roydon/reggie/mapper/AddressBookMapper.java
@@ -0,0 +1,10 @@
+package com.roydon.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.roydon.reggie.entity.AddressBook;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+
+}
