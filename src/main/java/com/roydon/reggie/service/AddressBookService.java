5ca922128a466455987aec459c6002396399485d
roydonGuo <3133010060@qq.com>
2022-10-01 22:35:49 +0800

    AddressBookService

---
+++ src/main/java/com/roydon/reggie/service/AddressBookService.java
@@ -0,0 +1,8 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+
+}
