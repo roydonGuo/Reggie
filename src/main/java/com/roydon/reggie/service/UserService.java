2be4386331a079a0438657426f32c763a76da8c4
roydonGuo <3133010060@qq.com>
2022-10-01 19:27:41 +0800

    UserService

---
+++ src/main/java/com/roydon/reggie/service/UserService.java
@@ -0,0 +1,7 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.User;
+
+public interface UserService extends IService<User> {
+}
