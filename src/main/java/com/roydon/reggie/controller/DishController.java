48abcaa61b1aa79a4edf51f42224d3718b9074b9
roydonGuo <3133010060@qq.com>
2022-10-01 19:23:50 +0800

    菜品

--- src/main/java/com/roydon/reggie/controller/DishController.java
+++ src/main/java/com/roydon/reggie/controller/DishController.java
@@ -127,5 +127,23 @@ public class DishController {
         return R.success("新增菜品成功");
     }
 
+    /**
+     * 根据条件查询对应的菜品数据
+     *
+     * @param dish
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Dish>> list(Dish dish) {
+
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
+        queryWrapper.eq(Dish::getStatus, 1);
+        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
+        List<Dish> dishList = dishService.list(queryWrapper);
+        return R.success(dishList);
+
+    }
+
 
 }
