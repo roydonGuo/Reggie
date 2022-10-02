430f974ca7a787b370a673031be567f67aa2015b
roydonGuo <3133010060@qq.com>
2022-10-02 19:22:21 +0800

    启动

--- src/main/java/com/roydon/reggie/ReggieApplication.java
+++ src/main/java/com/roydon/reggie/ReggieApplication.java
@@ -15,8 +15,9 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class ReggieApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
-        log.info("<=====项目启动成功=====>");
+        log.info("项目启动成功...");
     }
 }
