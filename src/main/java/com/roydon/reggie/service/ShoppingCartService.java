f881b7767477cbe69627536acc29cb0466d7d931
roydonGuo <3133010060@qq.com>
2022-10-02 18:17:05 +0800

    购物车

---
+++ src/main/java/com/roydon/reggie/service/ShoppingCartService.java
@@ -0,0 +1,8 @@
+package com.roydon.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roydon.reggie.entity.ShoppingCart;
+
+public interface ShoppingCartService extends IService<ShoppingCart> {
+
+}
